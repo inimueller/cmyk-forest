9a8d22b66dbb68d5adb2e04e4c0129f082a0ffa5
Ini Mueller <mail@inimueller.com>
2020-12-13 23:25:47 +0100

    creating users_list

--- lists/index.js
+++ lists/index.js
@@ -1,9 +1,11 @@
-const Stuff = require('./stuff')
-const FAQ = require('./faq')
+const Stuff = require("./stuff");
+const FAQ = require("./faq");
+const Users = require("./users");
 
 const loadLists = (keystone) => {
-  Stuff(keystone)
-  FAQ(keystone)
-}
+    Stuff(keystone);
+    FAQ(keystone);
+    Users(keystone);
+};
 
-module.exports = loadLists
+module.exports = loadLists;
