573d1833b97c7de3207332a87196cb32d645f48d
Ini Mueller <mail@inimueller.com>
2020-12-15 19:27:26 +0100

    created groups list

--- lists/index.js
+++ lists/index.js
@@ -1,9 +1,11 @@
-const Stuff = require('./stuff')
-const FAQ = require('./faq')
+const Stuff = require("./stuff");
+const FAQ = require("./faq");
+const Group = require("./group");
 
 const loadLists = (keystone) => {
-  Stuff(keystone)
-  FAQ(keystone)
-}
+    Stuff(keystone);
+    FAQ(keystone);
+    Group(keystone);
+};
 
-module.exports = loadLists
+module.exports = loadLists;
