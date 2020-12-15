b959cf72caae953a23d42ff9badcce56ef7f9c87
Manuel <manuelejdelatorre@gmail.com>
2020-12-15 16:38:22 -0300

    List project_details creado. deje comentado dato de FK por dudas

--- lists/index.js
+++ lists/index.js
@@ -1,9 +1,11 @@
 const Stuff = require('./stuff')
 const FAQ = require('./faq')
+const projectDetails = require('./projectDetail')
 
 const loadLists = (keystone) => {
   Stuff(keystone)
   FAQ(keystone)
+  projectDetails(keystone)
 }
 
 module.exports = loadLists
